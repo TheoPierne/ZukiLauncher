59f62935d8b026625399cfd46c37d1a2fc11fb86
TheoPierne <theo.pierne2@gmail.com>
2022-04-24 23:09:24 +0200

    Push v0.0.17-alpha

--- app/assets/js/configmanager.js
+++ app/assets/js/configmanager.js
@@ -279,6 +279,7 @@ exports.getClientToken = function(){
  * @param {string} clientToken The launcher's new Client Token.
  */
 exports.setClientToken = function(clientToken){
+    console.trace()
     config.clientToken = clientToken
 }
 
